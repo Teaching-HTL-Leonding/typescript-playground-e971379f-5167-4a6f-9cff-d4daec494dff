a33f573e946fcd0315a0fb4b689b481fb03b5525
Rainer Stropek <rainer@timecockpit.com>
2024-09-19 09:09:08 +0200

    Saved in TypeScript Playground at 2024-09-19T07:09:08.578Z

---
+++ cat/index.ts
@@ -0,0 +1,37 @@
+function setup() {
+    createCanvas(120, 150);
+
+    background("cyan");
+
+    fill("grey");
+    circle(60, 80, 80);
+
+    triangle(20, 50, 40, 40, 20, 20);
+
+    triangle(80, 40, 100, 50, 100, 20);
+
+    fill("pink")
+    triangle(55, 80, 65, 80, 60, 85);
+
+    fill("black");
+    circle(45, 70, 5);
+    circle(75, 70, 5);
+
+    line(30, 80, 45, 80);
+    line(75, 80, 90, 80);
+    line(30, 90, 45, 85);
+    line(90, 90, 75, 85);
+
+    line(60, 85, 60, 95);
+
+    line(60, 95, 50, 100);
+    line(60, 95, 70, 100);
+
+    line()
+    
+
+
+
+
+
+}
