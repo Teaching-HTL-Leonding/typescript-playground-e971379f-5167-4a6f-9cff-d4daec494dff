10f53d1d63dd2c6262eb5e11b81e76060bbfe358
Rainer Stropek <rainer@timecockpit.com>
2024-09-19 09:29:13 +0200

    Saved in TypeScript Playground at 2024-09-19T07:29:13.159Z

--- cat/index.ts
+++ cat/index.ts
@@ -6,8 +6,8 @@ function setup() {
     fill("grey");
     circle(60, 80, 80);
 
+    // ears
     triangle(20, 50, 40, 40, 20, 20);
-
     triangle(80, 40, 100, 50, 100, 20);
 
     fill("pink")
@@ -17,13 +17,14 @@ function setup() {
     circle(45, 70, 5);
     circle(75, 70, 5);
 
+    // whiskers
     line(30, 80, 45, 80);
     line(75, 80, 90, 80);
     line(30, 90, 45, 85);
     line(90, 90, 75, 85);
 
+    // mouth
     line(60, 85, 60, 95);
-
     line(60, 95, 50, 100);
     line(60, 95, 70, 100);
 }
