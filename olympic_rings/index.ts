d3bcda5736faf00fe90bdb350e739c85c6abd7dd
Rainer Stropek <rainer@timecockpit.com>
2024-09-19 08:22:45 +0200

    Saved in TypeScript Playground at 2024-09-19T06:22:45.639Z

---
+++ olympic_rings/index.ts
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(1000, 1000);
+
+  background(wh);
+
+  stroke(blue);
+
+
+  circle(100, 100, 200);
+
+
+  // <<< Add your code here.
+  //     Only add code between the curly braces (`{}`).
+}
