172ea52ab0d728f55425138878ed530a8fa590f6
Rainer Stropek <rainer@timecockpit.com>
2024-09-26 09:33:01 +0200

    Saved in TypeScript Playground at 2024-09-26T07:33:01.219Z

---
+++ color_worm/index.ts
@@ -0,0 +1,22 @@
+function setup() {
+    createCanvas(500, 500);
+    colorMode(HSB);
+    background(0); //Another way of writing black
+    noStroke();
+}
+
+
+/*
+GLOBAL variable "color_hue". Although TypeScript does not require
+specifiying a data type, you MUST ALWAYS do it in this course for
+practicing purposes! */
+let color_hue: number = 0;
+
+function mouseMoved() {
+    //Draw a circle with the current hue
+    fill(color_hue, 100, 100);
+    circle(mouseX, mouseY, 40);
+
+    //Note the use of the % operator (modulo)
+    color_hue = (color_hue + 5) % 360;
+}
