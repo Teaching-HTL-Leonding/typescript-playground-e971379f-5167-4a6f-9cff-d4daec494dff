98b162c16c6aa880b11186fdc0667aa246c52f64
Rainer Stropek <rainer@timecockpit.com>
2024-09-26 10:47:49 +0200

    Saved in TypeScript Playground at 2024-09-26T08:47:49.190Z

--- string_concat/index.ts
+++ string_concat/index.ts
@@ -1,12 +1,20 @@
 function setup() {
   createCanvas(200, 200);
   background("lightblue");
+
 }
+
+let clickprotocol : string = `${Math.round (mouseX)}/${Math.round (mouseY)}`
 
 function mouseClicked() {
   background("lightblue");
+
   noStroke();
   fill("cyan");
   circle(mouseX, mouseY, 10);
+
+  fill("black");
+  text(clickprotocol, 10, 10, 100, 100);
+
 
 }
