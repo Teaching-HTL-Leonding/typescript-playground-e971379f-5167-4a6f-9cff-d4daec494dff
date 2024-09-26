3dc7a36a76e2d84ada85d7778cfcf9c329017df2
Rainer Stropek <rainer@timecockpit.com>
2024-09-26 19:09:25 +0200

    Saved in TypeScript Playground at 2024-09-26T17:09:25.141Z

---
+++ color_picker/index.ts
@@ -0,0 +1,18 @@
+function setup() {
+    createCanvas(360, 200);
+    colorMode(HSB);
+    background(0, 100, 100);
+    noStroke();
+}
+
+function mouseMoved() {
+        fill(mouseX, mouseY, 100);
+    rect(0, 0, 360, 100);
+}
+
+function mouseClicked() {
+    fill(mouseX, mouseY, 100);
+    rect(0, 100, 360, 100);
+    fill(0);
+    text(`H: ${Math.round(mouseX)}, S: ${Math.round(mouseY)}, B: 100`, 5, height - 5);
+}
