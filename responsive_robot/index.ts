35375e22d67adbd2759a2be90366468f29d6b23e
Rainer Stropek <rainer@timecockpit.com>
2024-09-26 09:22:05 +0200

    Saved in TypeScript Playground at 2024-09-26T07:22:04.787Z

--- responsive_robot/index.ts
+++ responsive_robot/index.ts
@@ -15,13 +15,13 @@ function setup() {
   rect(headX, headY, headWidth, headHeight);
 
   fill("black");
-  circle(headWidth - headWidth*0.25, headHeight - headHeight*0.25, x*0.2);
-  circle(headWidth + headWidth*0.25, headHeight - headHeight*0.25, x*0.2);
+  circle(headWidth - headWidth * 0.25, headHeight - headHeight * 0.25, x * 0.2);
+  circle(headWidth + headWidth * 0.25, headHeight - headHeight * 0.25, x * 0.2);
 
   fill("white");
-  rect(headWidth - headWidth*0.25, headHeight*1.2, 
-  headWidth*0.5, headHeight*0.1)
+  rect(headWidth - headWidth * 0.25, headHeight * 1.2,
+    headWidth * 0.5, headHeight * 0.1)
 
-  line(headWidth, headHeight*0.25, headWidth, headHeight*0.5)
-  circle(headWidth, headHeight*0.25, x*0.1)
+  line(headWidth, headHeight * 0.25, headWidth, headHeight * 0.5)
+  circle(headWidth, headHeight * 0.25, x * 0.1)
 }
