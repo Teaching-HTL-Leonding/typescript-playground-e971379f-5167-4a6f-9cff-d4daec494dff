a760829ce1813183cd03b33381c55cd991ba5ebd
Rainer Stropek <rainer@timecockpit.com>
2024-09-26 20:47:22 +0200

    Saved in TypeScript Playground at 2024-09-26T18:47:22.186Z

--- empty_playground/index.ts
+++ empty_playground/index.ts
@@ -1,36 +1,20 @@
 function setup() {
-    createCanvas(300, 300);
-    background("gold");
+  createCanvas(200, 200);
+  background("lightblue");
+  colorMode(HSB);
 }
+
+let globalVar: string = "0/0";
 
 function mouseClicked() {
-    noStroke();
-    fill("red");
-    circle(mouseX, mouseY, 30);
+  background("lightblue");
 
-    //triangle(mouseX - 20, mouseY- 10,
-    // mouseX, mouseY + 10,
-    // mouseX + 20, mouseY - 10)
+  noStroke();
+  fill("cyan");
+  circle(mouseX, mouseY, 10);
 
-    // Übermalen
-    fill("gold");
-    noStroke();
-    rect(0, height - 20, width / 2, height);
+  globalVar= globalVar + ", "+`${Math.round (mouseX)}/${Math.round (mouseY)}`
 
-    fill("black");
-    text(`X: ${mouseX}, Y: ${mouseY}`, 5, height - 5);
-}
-// event handler
-function mouseMoved() {
-    noStroke();
-    fill("green");
-    circle(mouseX, mouseY, 10);
-
-    fill("gold");
-    noStroke();
-    rect(width / 2, height - 20,
-    width / 2, 20);
-
-    fill("black");
-    text(`X: ${mouseX}, Y: ${mouseY}`, 200, height - 5);
+  fill(0);
+  text(globalVar, 10, 10, 190, 190);
 }
