63d825893713cdc883efa38d41859b542977db7d
Rainer Stropek <rainer@timecockpit.com>
2024-10-04 12:28:30 +0200

    Saved in TypeScript Playground at 2024-10-04T10:28:29.745Z

--- color_areas/index.ts
+++ color_areas/index.ts
@@ -3,17 +3,34 @@ function setup() {
     background("black");
     noStroke();
 
-    fill("yellow");
-    rect(0, 0, width/3, height);
+    fill("gold");
+    rect(0, 0, width / 3, height);
 
     fill("green");
-    rect(width/3, 0, width/3, height);
+    rect(width / 3, 0, width / 3, height);
 
     fill("red");
-    rect(width- width73, 0, width/3, height);
+    rect(width - width / 3, 0, width / 3, height);
 
     fill("black")
+    rect(0, height - height / 4.5, width, height / 4.5);
     // <<< Add your code here
 }
 
+
+function mouseMoved() {
+    let message: string;
+
+    if (mouseX < width / 3) {
+        message = "Yellow";
+    }else if (mouseX < width / 3 * 2) {
+        message = "Green";
+    }
+    else {
+        message = "Red";
+    }
+
+    stroke("white");
+    text(`${message}`, width/2, height-height/10);
+}
 // <<< Add the function `moveClicked` with the required code here
