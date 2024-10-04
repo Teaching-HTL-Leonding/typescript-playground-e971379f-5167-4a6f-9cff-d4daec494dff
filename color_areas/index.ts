f9d5b06af74d6140de14fa424b124bcde2befd23
Rainer Stropek <rainer@timecockpit.com>
2024-10-04 12:17:29 +0200

    Saved in TypeScript Playground at 2024-10-04T10:17:28.918Z

---
+++ color_areas/index.ts
@@ -0,0 +1,19 @@
+function setup() {
+    createCanvas(400, 200);
+    background("black");
+    noStroke();
+
+    fill("yellow");
+    rect(0, 0, width/3, height);
+
+    fill("green");
+    rect(width/3, 0, width/3, height);
+
+    fill("red");
+    rect(width- width73, 0, width/3, height);
+
+    fill("black")
+    // <<< Add your code here
+}
+
+// <<< Add the function `moveClicked` with the required code here
