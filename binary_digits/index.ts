898f9730dd0e841722b1da9e6512bbfef08f68a7
Rainer Stropek <rainer@timecockpit.com>
2024-10-10 10:14:27 +0200

    Saved in TypeScript Playground at 2024-10-10T08:14:27.553Z

--- binary_digits/index.ts
+++ binary_digits/index.ts
@@ -19,19 +19,6 @@ function setup() {
   rect(9 * width / 13, height / 3, width / 12, height / 4);
   rect(11 * width / 13, height / 3, width / 12, height / 4);
 
- /* const lastDigit = num % 10;
-  const numWithoutLastDigit = Math.floor(num / 10);
-  const lastDigit2 = num % 100;
-  const numWithout2 = Math.floor(lastDigit2 / 10);
-  const lastDigit3 = num % 1000;
-  const numWo3 = Math.floor(lastDigit3 / 100);
-  const lastDigit4 = num % 10000;
-  const numWo4 = Math.floor(lastDigit4 / 1000);
-  const lastDigit5 = num % 100000;
-  const numWo5 = Math.floor(lastDigit5 / 10000);
-  const firstd = num % 1000000;
-  const firstdd = Math.floor(firstd / 100000);*/
-
   const lastDigit = num % 2;
   const numWithoutLastDigit = Math.floor(num / 10);
   const numWithout2 = Math.floor(num/2 %2);
