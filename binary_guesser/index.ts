d2e3f36dfcf9719e61fcc39314516128208f157a
Rainer Stropek <rainer@timecockpit.com>
2024-10-15 18:45:53 +0200

    Saved in TypeScript Playground at 2024-10-15T16:45:52.770Z

--- binary_guesser/index.ts
+++ binary_guesser/index.ts
@@ -1,4 +1,4 @@
-let num :any = 0;
+let num = 0;
 
 function setup() {
   num = Math.floor(random(0, 16));
@@ -25,44 +25,47 @@ function setup() {
   text(0, 5.5 * width / 9, height / 2 - 7);
 }
 
-
+let lastn = 0;
+let slastn = 0;
+let sn = 0;
+let firstn = 0;
 
 function mouseClicked() {
-let lastn :any = 0;
-let slastn :any = 0;
-let sn :any = 0;
-let firstn :any = 0;
 
   if (mouseX > width / 9 && mouseX < 3 * width / 9 && mouseY > height / 3 && mouseY < height / 3 + height / 4) {
   fill("black"); stroke ("yellow");
   rect(width / 9, height / 3, width / 9, height / 4);
   fill("yellow");
-   lastn ="1";
-    text(`${lastn}`, 1.5 * width / 9, height / 2 - 7);
+  
+  firstn = (firstn +1 ) %2;;
+    text(`${firstn}`, 1.5 * width / 9, height / 2 - 7);
   }
   else if (mouseX > 3 * width / 9 && mouseX < 4 * width / 9 && mouseY > height / 3 && mouseY < height / 3 + height / 4) {
   fill("black"); stroke ("yellow");
   rect(3 * width / 9, height / 3, width / 9, height / 4);
   fill("yellow");
-   slastn = "1";
-    text(`${slastn}`, 3.5 * width / 9, height / 2 - 7);
+    sn = (sn+1) %2;
+    text(`${sn}`, 3.5 * width / 9, height / 2 - 7);
   }
   else if (mouseX > 5 * width / 9 && mouseX < 6 * width / 9 && mouseY > height / 3 && mouseY < height / 3 + height / 4) {
   fill("black"); stroke ("yellow");
   rect(5 * width / 9, height / 3, width / 9, height / 4);
   fill("yellow");
-   sn ="1";
-    text(`${sn}`, 5.5 * width / 9, height / 2 - 7);
+   slastn =(slastn +1)%2;
+    text(`${slastn}`, 5.5 * width / 9, height / 2 - 7);
   }
   else if (mouseX > 7 * width / 9 && mouseX < 8 * width / 9 && mouseY > height / 3 && mouseY < height / 3 + height / 4) {
   fill("black"); stroke ("yellow");
   rect(7 * width / 9, height / 3, width / 9, height / 4);
   fill("yellow");
-   firstn = "1"
-    text(`${firstn}`, 7.5 * width / 9, height / 2 - 7);
+   lastn = (lastn +1)%2;
+    text(`${lastn}`, 7.5 * width / 9, height / 2 - 7);
   }
 
 }
+
+
+
 /*
 function mouseClicked() {
   let message: string;
