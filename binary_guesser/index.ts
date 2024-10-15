157ac0e12d1346587d04d298e67f2d2cfd5b9371
Rainer Stropek <rainer@timecockpit.com>
2024-10-15 18:01:10 +0200

    Saved in TypeScript Playground at 2024-10-15T16:01:10.029Z

--- binary_guesser/index.ts
+++ binary_guesser/index.ts
@@ -13,11 +13,11 @@ function setup() {
   rect(5 * width / 9, height / 3, width / 9, height / 4);
   rect(7 * width / 9, height / 3, width / 9, height / 4);
 
-fill("yellow");
-noStroke();
-textAlign(CENTER);
-textSize(30);
-text(`${num} in binary?`, width/2, height/5);
+  fill("yellow");
+  noStroke();
+  textAlign(CENTER);
+  textSize(30);
+  text(`${num} in binary?`, width / 2, height / 5);
 
   text(0, 1.5 * width / 9, height / 2 - 7);
   text(0, 3.5 * width / 9, height / 2 - 7);
@@ -25,27 +25,33 @@ text(`${num} in binary?`, width/2, height/5);
   text(0, 5.5 * width / 9, height / 2 - 7);
 }
 
+const lastn = Math.floor(num/2 %2);
+const slastn = Math.floor(num/2/2 %2);
+const sn = Math.floor(num/2/2/2%2);
+const firstn = Math.floor(num/2/2/2/2%2);
+
 function mouseClicked() {
 
-      let message : string;
-if(mouseX>width/9 && mouseX<3*width/9 && mouseY > height/3 && mouseY< height/3 + height/4) {
-  message = "got it";
-}
-else if(mouseX>3*width/9 && mouseX<5*width/9 && mouseY > height/3 && mouseY< height/3 + height/4) {
-  message = "second";
-}
-else if(mouseX>5*width/9 && mouseX<7*width/9 && mouseY > height/3 && mouseY< height/3 + height/4) {
-  message = "third";
-}
-else if(mouseX>7*width/9 && mouseX<8*width/9 && mouseY > height/3 && mouseY< height/3 + height/4) {
-  message = "fourth";
-}
+  let message: string;
+  if (mouseX > width / 9 && mouseX < 3 * width / 9 && mouseY > height / 3 && mouseY < height / 3 + height / 4) {
+    message = "got it";
+  }
+  else if (mouseX > 3 * width / 9 && mouseX < 5 * width / 9 && mouseY > height / 3 && mouseY < height / 3 + height / 4) {
+    message = "second";
+  }
+  else if (mouseX > 5 * width / 9 && mouseX < 7 * width / 9 && mouseY > height / 3 && mouseY < height / 3 + height / 4) {
+    message = "third";
+  }
+  else if (mouseX > 7 * width / 9 && mouseX < 8 * width / 9 && mouseY > height / 3 && mouseY < height / 3 + height / 4) {
+    message = "fourth";
+  }
 
+  else {
+    message = "";
+  }
 
-
-
-    fill("white");
-    textSize(30);
-    textAlign(CENTER, CENTER);
-    text(`${message}`, width/2, height/2)
+  fill("white");
+  textSize(30);
+  textAlign(CENTER, CENTER);
+  text(`${message}`, width / 2, height / 2)
 }
