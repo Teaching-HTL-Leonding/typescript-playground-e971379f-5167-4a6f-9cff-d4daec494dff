6cd053257f9dcb1ba7b9e7bfa817f23dc58d0ec2
Rainer Stropek <rainer@timecockpit.com>
2024-10-15 18:31:05 +0200

    Saved in TypeScript Playground at 2024-10-15T16:31:05.383Z

--- binary_guesser/index.ts
+++ binary_guesser/index.ts
@@ -25,6 +25,44 @@ function setup() {
   text(0, 5.5 * width / 9, height / 2 - 7);
 }
 
+
+
+function mouseClicked() {
+let lastn :any = 0;
+let slastn :any = 0;
+let sn :any = 0;
+let firstn :any = 0;
+
+  if (mouseX > width / 9 && mouseX < 3 * width / 9 && mouseY > height / 3 && mouseY < height / 3 + height / 4) {
+  fill("black"); stroke ("yellow");
+  rect(width / 9, height / 3, width / 9, height / 4);
+  fill("yellow");
+   lastn ="1";
+    text(`${lastn}`, 1.5 * width / 9, height / 2 - 7);
+  }
+  else if (mouseX > 3 * width / 9 && mouseX < 4 * width / 9 && mouseY > height / 3 && mouseY < height / 3 + height / 4) {
+  fill("black"); stroke ("yellow");
+  rect(3 * width / 9, height / 3, width / 9, height / 4);
+  fill("yellow");
+   slastn = "1";
+    text(`${slastn}`, 3.5 * width / 9, height / 2 - 7);
+  }
+  else if (mouseX > 5 * width / 9 && mouseX < 6 * width / 9 && mouseY > height / 3 && mouseY < height / 3 + height / 4) {
+  fill("black"); stroke ("yellow");
+  rect(5 * width / 9, height / 3, width / 9, height / 4);
+  fill("yellow");
+   sn ="1";
+    text(`${sn}`, 5.5 * width / 9, height / 2 - 7);
+  }
+  else if (mouseX > 7 * width / 9 && mouseX < 8 * width / 9 && mouseY > height / 3 && mouseY < height / 3 + height / 4) {
+  fill("black"); stroke ("yellow");
+  rect(7 * width / 9, height / 3, width / 9, height / 4);
+  fill("yellow");
+   firstn = "1"
+    text(`${firstn}`, 7.5 * width / 9, height / 2 - 7);
+  }
+
+}
 /*
 function mouseClicked() {
   let message: string;
@@ -52,31 +90,3 @@ function mouseClicked() {
 
 
 
-
-
-
-function mouseClicked() {
-let lastn :any = 0;
-let slastn :any = 0;
-let sn :any = 0;
-let firstn :any = 0;
-
-  fill("yellow");
-  if (mouseX > width / 9 && mouseX < 3 * width / 9 && mouseY > height / 3 && mouseY < height / 3 + height / 4) {
-   lastn ="1";
-    text(`${lastn}`, 1.5 * width / 9, height / 2 - 7);
-  }
-  else if (mouseX > 3 * width / 9 && mouseX < 4 * width / 9 && mouseY > height / 3 && mouseY < height / 3 + height / 4) {
-   slastn = "1";
-    text(`${slastn}`, 3.5 * width / 9, height / 2 - 7);
-  }
-  else if (mouseX > 5 * width / 9 && mouseX < 6 * width / 9 && mouseY > height / 3 && mouseY < height / 3 + height / 4) {
-   sn ="1";
-    text(`${sn}`, 5.5 * width / 9, height / 2 - 7);
-  }
-  else if (mouseX > 7 * width / 9 && mouseX < 8 * width / 9 && mouseY > height / 3 && mouseY < height / 3 + height / 4) {
-   firstn = "1"
-    text(`${firstn}`, 7.5 * width / 9, height / 2 - 7);
-  }
-
-}
