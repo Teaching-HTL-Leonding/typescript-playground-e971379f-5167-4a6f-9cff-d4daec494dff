a87a6da9c387454d3afe4e66445f2b78748db112
Rainer Stropek <rainer@timecockpit.com>
2024-10-17 10:46:12 +0200

    Saved in TypeScript Playground at 2024-10-17T08:46:11.792Z

--- animation_basics/index.ts
+++ animation_basics/index.ts
@@ -72,7 +72,7 @@ function draw() {
 
     // Move the sprite to the right. If the sprite moved too far, set it back to the left.
     x += 5;
-    if(x === 500){
+    if(x > 500){
         x = -500;
     }
     // <<< Add the logic to reset the sprite position to the left when it has moved too far to the right (> 500 pixels is a good threshold).
