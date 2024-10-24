4d221d2a740bc8a8ab362a7f94a1eab067183848
Rainer Stropek <rainer@timecockpit.com>
2024-10-24 10:48:44 +0200

    Saved in TypeScript Playground at 2024-10-24T08:48:44.064Z

--- random_cards/index.ts
+++ random_cards/index.ts
@@ -20,6 +20,18 @@ function preload() {
   cardIx = Math.floor(random(1, 14));
 switch(cardIx){
   case 1: cardIx = ; break;
+  case 2: cardIx = ; break;
+  case 3: cardIx = ; break;
+  case 4: cardIx = ; break;
+  case 5: cardIx = ; break;
+  case 6: cardIx = ; break;
+  case 7: cardIx = ; break;
+  case 8: cardIx = ; break;
+  case 9: cardIx = ; break;
+  case 10: cardIx = ; break;
+  case 11: cardIx = ; break;
+  case 12: cardIx = ; break;
+  case 13: cardIx = ; break;
 }
   cardImage = loadImage(`${BASE_URL}/${colorname}/${cardIx}`)
 
