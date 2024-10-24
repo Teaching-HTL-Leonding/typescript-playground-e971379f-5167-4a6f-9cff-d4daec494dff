d45516a458333e90ca5ec079a25f5b4fa57a1ac4
Rainer Stropek <rainer@timecockpit.com>
2024-10-24 08:51:25 +0200

    Saved in TypeScript Playground at 2024-10-24T06:51:25.315Z

--- random_cards/index.ts
+++ random_cards/index.ts
@@ -19,10 +19,10 @@ function preload() {
     case 0 && 7: cardImage = loadImage(`${BASE_URL}/clubs/7.png.`); break;
     case 0 && 8: cardImage = loadImage(`${BASE_URL}/clubs/8.png.`); break;
     case 0 && 9: cardImage = loadImage(`${BASE_URL}/clubs/9.png.`); break;
-    case 0 && 10: cardImage = loadImage(`$BASE_URL}/clubs/10.png.`); break;
-    case 0 && 11: cardImage = loadImage(`$BASE_URL}/clubs/J.png.`); break;
-    case 0 && 12: cardImage = loadImage(`$BASE_URL}/clubs/Q.png.`); break;
-    case 0 && 13: cardImage = loadImage(`$BASE_URL}/clubs/K.png.`); break;
+    case 0 && 10: cardImage = loadImage(`${BASE_URL}/clubs/10.png.`); break;
+    case 0 && 11: cardImage = loadImage(`${BASE_URL}/clubs/J.png.`); break;
+    case 0 && 12: cardImage = loadImage(`${BASE_URL}/clubs/Q.png.`); break;
+    case 0 && 13: cardImage = loadImage(`${BASE_URL}/clubs/K.png.`); break;
 
     case 1 && 1: cardImage = loadImage(`${BASE_URL}/diamonds/A.png.`); break;
     case 1 && 2: cardImage = loadImage(`${BASE_URL}/diamonds/2.png.`); break;
@@ -33,10 +33,10 @@ function preload() {
     case 1 && 7: cardImage = loadImage(`${BASE_URL}/diamonds/7.png.`); break;
     case 1 && 8: cardImage = loadImage(`${BASE_URL}/diamonds/8.png.`); break;
     case 1 && 9: cardImage = loadImage(`${BASE_URL}/diamonds/9.png.`); break;
-    case 1 && 10: cardImage = loadImage(`$BASE_URL}/diamonds/10.png.`); break;
-    case 1 && 11: cardImage = loadImage(`$BASE_URL}/diamonds/J.png.`); break;
-    case 1 && 12: cardImage = loadImage(`$BASE_URL}/diamonds/Q.png.`); break;
-    case 1 && 13: cardImage = loadImage(`$BASE_URL}/diamonds/K.png.`); break;
+    case 1 && 10: cardImage = loadImage(`${BASE_URL}/diamonds/10.png.`); break;
+    case 1 && 11: cardImage = loadImage(`${BASE_URL}/diamonds/J.png.`); break;
+    case 1 && 12: cardImage = loadImage(`${BASE_URL}/diamonds/Q.png.`); break;
+    case 1 && 13: cardImage = loadImage(`${BASE_URL}/diamonds/K.png.`); break;
 
     case 2 && 1: cardImage = loadImage(`${BASE_URL}/hearts/A.png.`); break;
     case 2 && 2: cardImage = loadImage(`${BASE_URL}/hearts/2.png.`); break;
@@ -47,10 +47,10 @@ function preload() {
     case 2 && 7: cardImage = loadImage(`${BASE_URL}/hearts/7.png.`); break;
     case 2 && 8: cardImage = loadImage(`${BASE_URL}/hearts/8.png.`); break;
     case 2 && 9: cardImage = loadImage(`${BASE_URL}/hearts/9.png.`); break;
-    case 2 && 10: cardImage = loadImage(`$BASE_URL}/clubs/10.png.`); break;
-    case 2 && 11: cardImage = loadImage(`$BASE_URL}/clubs/J.png.`); break;
-    case 2 && 12: cardImage = loadImage(`$BASE_URL}/clubs/Q.png.`); break;
-    case 2 && 13: cardImage = loadImage(`$BASE_URL}/clubs/K.png.`); break;
+    case 2 && 10: cardImage = loadImage(`${BASE_URL}/clubs/10.png.`); break;
+    case 2 && 11: cardImage = loadImage(`${BASE_URL}/clubs/J.png.`); break;
+    case 2 && 12: cardImage = loadImage(`${BASE_URL}/clubs/Q.png.`); break;
+    case 2 && 13: cardImage = loadImage(`${BASE_URL}/clubs/K.png.`); break;
 
     case 3 && 1: cardImage = loadImage(`${BASE_URL}/spades/A.png.`); break;
     case 3 && 2: cardImage = loadImage(`${BASE_URL}/spades/2.png.`); break;
@@ -61,10 +61,10 @@ function preload() {
     case 3 && 7: cardImage = loadImage(`${BASE_URL}/spades/7.png.`); break;
     case 3 && 8: cardImage = loadImage(`${BASE_URL}/spades/8.png.`); break;
     case 3 && 9: cardImage = loadImage(`${BASE_URL}/spades/9.png.`); break;
-    case 3 && 10: cardImage = loadImage(`$BASE_URL}/spades/10.png.`); break;
-    case 3 && 11: cardImage = loadImage(`$BASE_URL}/spades/J.png.`); break;
-    case 3 && 12: cardImage = loadImage(`$BASE_URL}/spades/Q.png.`); break;
-    case 3 && 13: cardImage = loadImage(`$BASE_URL}/spades/K.png.`); break;
+    case 3 && 10: cardImage = loadImage(`${BASE_URL}/spades/10.png.`); break;
+    case 3 && 11: cardImage = loadImage(`${BASE_URL}/spades/J.png.`); break;
+    case 3 && 12: cardImage = loadImage(`${BASE_URL}/spades/Q.png.`); break;
+    case 3 && 13: cardImage = loadImage(`${BASE_URL}/spades/K.png.`); break;
   }3  // <<< Add code here
 }
 
