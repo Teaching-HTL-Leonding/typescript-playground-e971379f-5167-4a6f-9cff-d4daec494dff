31b5af59a3c3153b0300760302c6bed6cd526eed
Rainer Stropek <rainer@timecockpit.com>
2024-10-24 10:50:11 +0200

    Saved in TypeScript Playground at 2024-10-24T08:50:11.483Z

--- random_cards/index.ts
+++ random_cards/index.ts
@@ -3,7 +3,7 @@ let cardImage: p5.Image;
 const BASE_URL = 'https://cddataexchange.blob.core.windows.net/images/cards';
 
 let colorIx: number;
-let cardIx: number;
+let cards: number;
 
 function preload() {
   // Generate random color (0 = clubs, 1 = diamonds, 2 = hearts, 3 = spades)
@@ -17,23 +17,24 @@ function preload() {
   }
 
   // Generate random card (1 = Ace, 2 = 2, ..., 10 = 10, 11 = Jack, 12 = Queen, 13 = King)
-  cardIx = Math.floor(random(1, 14));
-switch(cardIx){
-  case 1: cardIx = ; break;
-  case 2: cardIx = ; break;
-  case 3: cardIx = ; break;
-  case 4: cardIx = ; break;
-  case 5: cardIx = ; break;
-  case 6: cardIx = ; break;
-  case 7: cardIx = ; break;
-  case 8: cardIx = ; break;
-  case 9: cardIx = ; break;
-  case 10: cardIx = ; break;
-  case 11: cardIx = ; break;
-  case 12: cardIx = ; break;
-  case 13: cardIx = ; break;
+  cards = Math.floor(random(1, 14));
+  cards : any;
+switch(cards){
+  case 1: cards = A; break;
+  case 2: cards = 2; break;
+  case 3: cards = 3; break;
+  case 4: cards = 4; break;
+  case 5: cards = 5; break;
+  case 6: cards = 6; break;
+  case 7: cards = 7; break;
+  case 8: cards = 8; break;
+  case 9: cards = 9; break;
+  case 10: cards = 10; break;
+  case 11: cards = J; break;
+  case 12: cards = Q; break;
+  case 13: cards = K; break;
 }
-  cardImage = loadImage(`${BASE_URL}/${colorname}/${cardIx}`)
+  cardImage = loadImage(`${BASE_URL}/${colorname}/${cards}`)
 
 }
 
@@ -45,5 +46,5 @@ function setup() {
   image(cardImage, width / 2, height / 2);
 
     text(colorIx, 10, 20);
-  text(cardIx, 10, 40);
+  text(cards, 10, 40);
 }
