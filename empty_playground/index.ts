b381363cab25e5e079df1a039d28c7fee4283852
Rainer Stropek <rainer@timecockpit.com>
2024-11-08 12:19:11 +0100

    Saved in TypeScript Playground at 2024-11-08T11:19:11.112Z

--- empty_playground/index.ts
+++ empty_playground/index.ts
@@ -1,4 +1,18 @@
 function setup() {
+    createCanvas(250, 250);
+    background("black");
+
+    strokeWeight(5);
+    strokeCap(SQUARE);
+
+    let v = 25;
+    while (v <= 225) {
+        stroke("yellow");
+        line(25, v, 225, v);
+        stroke("blue");
+        line(v, 25, v, 225);
+        v += 50;
+    }
 }
 
 function draw() {
