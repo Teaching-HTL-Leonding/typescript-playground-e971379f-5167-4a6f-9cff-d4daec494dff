312c04b87abf73efd69870ffad660aad9e3b16e4
Rainer Stropek <rainer@timecockpit.com>
2024-11-28 20:46:39 +0100

    Saved in TypeScript Playground at 2024-11-28T19:46:38.817Z

--- empty_playground/index.ts
+++ empty_playground/index.ts
@@ -1,18 +1,31 @@
 function setup() {
-    createCanvas(250, 250);
-    background("black");
+    createCanvas(400, 400);
 
-    strokeWeight(1);
-    stroke("yellow");
-    colorMode(HSB);
+    const meinText = ["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"];
+    let summe: number = 0;
 
-    let y = 225;
-    while (y >= 25) {
-        stroke(y, 100, 100);
-        line(25, y, y, 225);
-        line(y, 25, 225, y);
-        y -= 12.5;
+    for (let i = 0; i < meinText.length; i++) {
+        text(meinText[i], 1, (i + 1) * 10);
+
+        let txt = meinText[i];
+        let digits = "";
+        for (let j = 0; j < txt.length; j++) {
+            const myNumber = Number(txt.charAt(j));
+            if (Number.isInteger(myNumber)) {
+                digits += txt.charAt(j);
+            }
+        }
+        if (digits.length > 2) {
+            digits = digits.charAt(0) + digits.charAt(digits.length - 1);
+        }
+        if (digits.length < 2) {
+            digits = digits + digits;
+        }
+        text(digits, 100, 10 * (i + 1));
+        summe += Number(digits);
     }
+
+    text(summe, 150, 50);
 }
 
 function draw() {
