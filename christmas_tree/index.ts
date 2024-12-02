b0957169dbf5507416bff08f62e3295eb8f426a8
Rainer Stropek <rainer@timecockpit.com>
2024-12-02 15:24:48 +0100

    Saved in TypeScript Playground at 2024-12-02T14:24:48.065Z

--- christmas_tree/index.ts
+++ christmas_tree/index.ts
@@ -14,21 +14,25 @@ function setup() {
 
   let trianglebase = INITIAL_TRIANGLE_BASE_WIDTH;
   let triangleheight = trianglebase * HEIGHT_TO_WIDTH_RATIO;
-  translate(width/2, TOP_MARGIN+triangleheight)
+  translate(width / 2, TOP_MARGIN + triangleheight);
 
-{  for(let i = 0; i < NUMBER_OF_LAYERS; i++){
-  fill("green");
-    triangle(-trianglebase/2, 0, trianglebase/2, 0, 0, -triangleheight);
-    fill(Math.floor(random(0, 256)), Math.floor(random(0, 256)), Math.floor(random(0, 256)))
-    circle(trianglebase/2, SPHERE_DIAMETER/2, SPHERE_DIAMETER);
-    circle(-trianglebase/2, SPHERE_DIAMETER/2, SPHERE_DIAMETER);
+  {
+    for (let i = 0; i < NUMBER_OF_LAYERS; i++) {
+      fill("green");
+      triangle(-trianglebase / 2, 0, trianglebase / 2, 0, 0, -triangleheight);
+      fill(Math.floor(random(0, 256)), Math.floor(random(0, 256)), Math.floor(random(0, 256)))
+      circle(trianglebase / 2, SPHERE_DIAMETER / 2, SPHERE_DIAMETER);
+      circle(-trianglebase / 2, SPHERE_DIAMETER / 2, SPHERE_DIAMETER);
 
-    if(i <NUMBER_OF_LAYERS - 1){translate(0, triangleheight);}
-    trianglebase = trianglebase*1.5;
-    triangleheight = trianglebase * HEIGHT_TO_WIDTH_RATIO;
-  }}
-  
+      if (i < NUMBER_OF_LAYERS - 1) {
+        translate(0, triangleheight);
+        trianglebase = trianglebase * 1.5;
+        triangleheight = trianglebase * HEIGHT_TO_WIDTH_RATIO;
+      }
+    }
+  }
+
   fill("brown");
-  rect(-TRUNK_WIDTH/2, 0, TRUNK_WIDTH, TRUNK_HEIGHT);
+  rect(-TRUNK_WIDTH / 2, 0, TRUNK_WIDTH, TRUNK_HEIGHT);
   // <<< Draw the tree
 }
