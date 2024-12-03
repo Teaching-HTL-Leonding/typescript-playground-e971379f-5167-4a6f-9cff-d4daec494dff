a5775114e2faa882c53daf67d053227f8c9e9f18
Rainer Stropek <rainer@timecockpit.com>
2024-12-03 16:20:36 +0100

    Saved in TypeScript Playground at 2024-12-03T15:20:35.543Z

--- christmas_tree/index.ts
+++ christmas_tree/index.ts
@@ -10,34 +10,8 @@ const TRUNK_HEIGHT = 50;
 function setup() {
   createCanvas(500, 500);
   background("black");
+  fill("green");
   noStroke();
 
-
-  for (let g = 0; g < 2; g++) {
-    resetMatrix();
-    let trianglebase = INITIAL_TRIANGLE_BASE_WIDTH;
-    let triangleheight = trianglebase * HEIGHT_TO_WIDTH_RATIO;
-    translate(width / 2, TOP_MARGIN + triangleheight);
-    for (let i = 0; i < NUMBER_OF_LAYERS; i++) {
-      if (g === 0) {
-        fill("green");
-        triangle(-trianglebase / 2, 0, trianglebase / 2, 0, 0, -triangleheight);
-      }
-      else {
-        fill(Math.floor(random(0, 256)), Math.floor(random(0, 256)), Math.floor(random(0, 256)))
-        circle(trianglebase / 2, SPHERE_DIAMETER / 2, SPHERE_DIAMETER);
-        circle(-trianglebase / 2, SPHERE_DIAMETER / 2, SPHERE_DIAMETER);
-      }
-
-      if (i < NUMBER_OF_LAYERS - 1) {
-        translate(0, triangleheight);
-        trianglebase = trianglebase * 1.5;
-        triangleheight = trianglebase * HEIGHT_TO_WIDTH_RATIO;
-      }
-    }
-  }
-
-  fill("brown");
-  rect(-TRUNK_WIDTH / 2, 0, TRUNK_WIDTH, TRUNK_HEIGHT);
   // <<< Draw the tree
 }
