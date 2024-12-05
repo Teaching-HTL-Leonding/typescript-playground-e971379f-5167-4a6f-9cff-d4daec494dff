b02dd93222256ec1da61d241ee6ee10af6eb000c
Rainer Stropek <rainer@timecockpit.com>
2024-12-05 08:32:08 +0100

    Saved in TypeScript Playground at 2024-12-05T07:32:08.491Z

--- patterns_with_lines/index.ts
+++ patterns_with_lines/index.ts
@@ -8,6 +8,7 @@ function setup() {
     textSize(50);
     textAlign(CENTER, CENTER);
     text("Move the mouse to start", 0, 0, width, height);
+    colorMode(HSB);
 }
 
 function mouseMoved() {
@@ -16,7 +17,28 @@ function mouseMoved() {
     strokeWeight(2);
 
     noFill();
-    stroke("aqua");
+    let xscaling = Math.round((mouseX/width)*10);
 
+    for (let i = 0, a = 0; i < width; i++, a+=6) {
+    stroke(a, 100, 100);
+        push();
+        for (let g = 0; g < height; g+=10) {
+            line(0, 0, 10*xscaling, 5*xscaling);
+            line(0, 10*xscaling, 10*xscaling, 5*xscaling);
+            translate(0, 10*xscaling);
+        }
+        pop();
+        translate(10*xscaling, 0);
+    }
+    resetMatrix();
+
+    let xpercentage = Math.round((mouseX / width)*100);
+    noStroke();
+    fill("black");
+    rect(0, height - 25, width, 25);
+    fill("white");
+    textSize(12);
+    textAlign(LEFT, BOTTOM)
+    text(Math.round(mouseX) + ` of ${width} = ` + xpercentage + "%", 5, height - 5);
     // <<< Add your code here
 }
