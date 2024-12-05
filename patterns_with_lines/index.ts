246e1e2ff09e68a33833a548357ca96c4f7408c3
Rainer Stropek <rainer@timecockpit.com>
2024-12-05 08:51:37 +0100

    Saved in TypeScript Playground at 2024-12-05T07:51:37.407Z

--- patterns_with_lines/index.ts
+++ patterns_with_lines/index.ts
@@ -17,19 +17,20 @@ function mouseMoved() {
     strokeWeight(2);
 
     noFill();
-    let xscaling = Math.round((mouseX/width)*10);
+    let xscaling = ((mouseX/10000)*10);
 
+{
     for (let i = 0, a = 0; i < width; i++, a+=6) {
     stroke(a, 100, 100);
         push();
         for (let g = 0; g < height; g+=10) {
-            line(0, 0, 10*xscaling, 5*xscaling);
-            line(0, 10*xscaling, 10*xscaling, 5*xscaling);
-            translate(0, 10*xscaling);
+            line(0, 0, 10 +10*xscaling, 5+5*xscaling);
+            line(0, 10+10*xscaling, 10+10*xscaling, 5+ 5*xscaling);
+            translate(0, 10+10*xscaling);
         }
         pop();
-        translate(10*xscaling, 0);
-    }
+        translate(10+10*xscaling, 0);
+    }}
     resetMatrix();
 
     let xpercentage = Math.round((mouseX / width)*100);
