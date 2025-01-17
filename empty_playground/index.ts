0497e786b108a39061c692114e589fb03e1ea031
Rainer Stropek <rainer@timecockpit.com>
2025-01-17 12:22:41 +0100

    Saved in TypeScript Playground at 2025-01-17T11:22:41.029Z

--- empty_playground/index.ts
+++ empty_playground/index.ts
@@ -1,33 +1,22 @@
 function setup() {
     createCanvas(400, 400);
+    background("black");
 
-    const meinText = ["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"];
-    let summe: number = 0;
-
-    for (let i = 0; i < meinText.length; i++) {
-        text(meinText[i], 1, (i + 1) * 10);
-
-        let txt = meinText[i];
-        let digits = "";
-        for (let j = 0; j < txt.length; j++) {
-            const myNumber = Number(txt.charAt(j));
-            if (Number.isInteger(myNumber)) {
-                digits += txt.charAt(j);
-            }
-        }
-        if (digits.length > 2) {
-            digits = digits.charAt(0) + digits.charAt(digits.length - 1);
-        }
-        if (digits.length < 2) {
-            digits = digits + digits;
-        }
-        text(digits, 100, 10 * (i + 1));
-        summe += Number(digits);
-        
+    for (let i = 0; i < 3; i++) {
+        drawCircle("yellow");
     }
-
-    text(summe, 150, 50);
 }
 
-function draw() {
+function mouseClicked() {
+    drawCircle("aqua");
+}
+
+function drawCircle(color: string) {
+    stroke("black");
+    fill(color);
+    let x = random(0, 400);
+    let y = random(0, 400);
+    let diameter = random(50, 250);
+    circle(x, y, diameter);
+    //console.info(x, y, diameter);
 }
