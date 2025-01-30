2ce6eb9cf45d53b535af30aef8bd84003f7bdef3
Rainer Stropek <rainer@timecockpit.com>
2025-01-30 10:16:06 +0100

    Saved in TypeScript Playground at 2025-01-30T09:16:06.399Z

--- bubbles_with_functions/index.ts
+++ bubbles_with_functions/index.ts
@@ -28,7 +28,7 @@ function draw() {
   textSize(10);
   text(`Score: ${points}`, 10, 10);
 
-  if(circles_diameter.length >=10){
+  if (circles_diameter.length >= 10) {
     gameover();
   }
 }
@@ -47,27 +47,27 @@ function gameover() {
   clearInterval(circle_interval);
   clearInterval(level_interval);
   fill("#272727");
-  rect(50, 50, width-100, height-100);
-  textAlign(CENTER,CENTER);
+  rect(50, 50, width - 100, height - 100);
+  textAlign(CENTER, CENTER);
   fill("white");
   noStroke();
   textSize(35);
-  text("✨", width/2, height/2-40);
+  text("✨", width / 2, height / 2 - 40);
   textSize(25);
-  text("GAME OVER", width/2, height/2);
+  text("GAME OVER", width / 2, height / 2);
   textSize(20);
-  text(`Final score: ${points}`, width/2, height/2 + 30);
+  text(`Final score: ${points}`, width / 2, height / 2 + 30);
 }
 
 function levels() {
   clearInterval(circle_interval);
-  waiting_time/= 2;
+  waiting_time /= 2;
   circle_interval = setInterval(addRandomCircle, waiting_time);
 }
 
 function addRandomCircle() {
   circles_diameter.push(random(10, 50));
-  circles_x.push(random(width)); 
+  circles_x.push(random(width));
   circles_y.push(random(height));
 }
 
@@ -77,4 +77,3 @@ function isInside(x: number, y: number, circle_index: number): boolean {
   const distance = Math.sqrt(distance_x * distance_x + distance_y * distance_y);
   return distance < circles_diameter[circle_index] / 2;
 }
-// <<< Add functions here
